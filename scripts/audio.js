479313d0610ca5b80dcd92d18e43d1fc3fe7ade8
rin-the-designer <me@rinchong.kim>
2024-11-26 02:17:19 -0500

    media edits

--- scripts/audio.js
+++ scripts/audio.js
@@ -2,6 +2,40 @@ const audioElement = document.querySelector("audio");
 const playButton = document.querySelector("#play");
 let isFirstPlay = true;
 
+audioElement.addEventListener('play', startMidiPlayback);
+audioElement.addEventListener('pause', pauseMidiPlayback);
+
+
+// Media Session API for audio controls and metadata
+if ('mediaSession' in navigator) {
+
+    navigator.mediaSession.metadata = new MediaMetadata({
+    title: 'Manifesto',
+    artist: 'Rin Kim',
+    album: 'Song by WonderlustBeats',
+    artwork: [
+        { src: '/assets/media-cover/cover-96.png',   sizes: '96x96',   type: 'image/png' },
+        { src: '/assets/media-cover/cover-128.png', sizes: '128x128', type: 'image/png' },
+        { src: '/assets/media-cover/cover-192.png', sizes: '192x192', type: 'image/png' },
+        { src: '/assets/media-cover/cover-256.png', sizes: '256x256', type: 'image/png' },
+        { src: '/assets/media-cover/cover-384.png', sizes: '384x384', type: 'image/png' },
+        { src: '/assets/media-cover/cover-512.png', sizes: '512x512', type: 'image/png' },
+    ]
+    });
+
+    navigator.mediaSession.setActionHandler('play', function() {
+        audioElement.play();
+        startWordDisplay();
+        playButton.textContent = "Pause";
+    });
+    
+    navigator.mediaSession.setActionHandler('pause', function() {
+        audioElement.pause();
+        pauseWordDisplay();
+        playButton.textContent = "Play";
+    });
+}
+
 function togglePlay() {
     const $intro = $('#intro');
 
